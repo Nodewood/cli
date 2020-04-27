67b5afab7bb6ab8f03ebef423403597af521df02
Dan Hulton <dan@danhulton.com>
2020-04-26 21:53:08 -0400

    Confirms project folder empty, gets apiKey & secretKey, writes .nodewood.js file.

---
+++ src/commands/new.js
@@ -0,0 +1,120 @@
+const chalk = require('chalk');
+const { get } = require('lodash');
+const { resolve } = require('path');
+const { prompt } = require('inquirer');
+const {
+  readdirSync,
+  emptyDirSync,
+  existsSync,
+  lstatSync,
+  writeJsonSync,
+} = require('fs-extra');
+const { Command } = require('../lib/Command');
+
+class NewCommand extends Command {
+  /**
+   * Returns the one-liner version of help text to display on the general help command.
+   *
+   * @return {String}
+   */
+  helpLine() {
+    return 'Intialize a new Nodewood project.';
+  }
+
+  /**
+   * Returns the full help text specific to the command.
+   *
+   * @return {String}
+   */
+  helpDetailed() {
+    console.log('Intialize a new Nodewood project.');
+
+    console.log(chalk.yellow('\nUsage:'));
+    console.log('  nodewood new DIR');
+  }
+
+  /**
+   * Execute the command.
+   *
+   * @param {Array} args - Command arguments, as parsed by minimist.
+   */
+  async execute(args) {
+    const path = resolve(process.cwd(), get(args._, 1, false));
+
+    if (! this.isEmptyDirectory(path)) {
+      console.log(chalk.red(`Directory '${path}' must be empty.`));
+      return;
+    }
+
+    emptyDirSync(path);
+
+    const { apiKey, secretKey } = await this.getApiKeys();
+
+    writeJsonSync(
+      resolve(path, '.nodewood.js'),
+      { apiKey, secretKey },
+      { spaces: 2 },
+    );
+
+    console.log('neat!');
+  }
+
+  /**
+   * Checks if the provided path is an empty directory (or does not exist).
+   *
+   * @param {String} path - The path to check.
+   *
+   * @return {Boolean}
+   */
+  isEmptyDirectory(path) {
+    if (! existsSync(path)) {
+      return true;
+    }
+
+    if (lstatSync(path).isDirectory()) {
+      return readdirSync(path).length === 0;
+    }
+
+    return false;
+  }
+
+  /**
+   * Gets the API and Secret keys from the user.
+   *
+   * @return {apiKey, secretKey}
+   */
+  async getApiKeys() {
+    const questions = [
+      {
+        name: 'apiKey',
+        type: 'input',
+        message: 'Enter the API Key for this project:',
+        validate: (value) => {
+          if (value.length) {
+            return true;
+          }
+
+          return `Visit ${chalk.cyan('https://nodewood.test/app/projects')} for your API key.`;
+        },
+      },
+      {
+        name: 'secretKey',
+        type: 'password',
+        message: 'Enter the Secret Key for this project:',
+        validate: (value) => {
+          if (value.length) {
+            return true;
+          }
+
+          return `Visit ${chalk.cyan('https://nodewood.test/app/projects')} for your secret key.`;
+        },
+      },
+    ];
+
+    return prompt(questions);
+  }
+}
+
+module.exports = {
+  NewCommand,
+};
