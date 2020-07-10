6676befb63f46bbaf33f02b05c137694050c8275
Dan Hulton <dan@danhulton.com>
2020-07-09 21:43:48 -0400

    Big refactor, moves load config/calculate differences stuff to a lib file, removes a lot of duplication.

--- src/commands/stripe.js
+++ src/commands/stripe.js
@@ -1,10 +1,14 @@
 const chalk = require('chalk');
-const stripe = require('stripe')(process.env.STRIPE_SK);
 const { resolve } = require('path');
-const { readJsonSync, writeJsonSync } = require('fs-extra');
-const { get, last, sortBy, omit, isEqual, flatMap } = require('lodash');
+const { get } = require('lodash');
 const { Command } = require('../lib/Command');
 const { isNodewoodProject } = require('../lib/file');
+const {
+  getLocalConfig,
+  writeLocalConfig,
+  getRemoteConfig,
+  calculateDifferences,
+} = require('../lib/stripe');
 
 class StripeCommand extends Command {
   /**
@@ -47,10 +51,8 @@ class StripeCommand extends Command {
     }
 
     // Load local and remote config for further processing
-    this.localConfig = this.getLocalConfig();
-    this.remoteConfig = await this.getRemoteConfig();
-
-    this.calculateDifferences();
+    this.localConfig = getLocalConfig();
+    this.remoteConfig = await getRemoteConfig();
 
     const type = get(args._, 1);
     if (type === 'diff') {
@@ -67,235 +69,9 @@ class StripeCommand extends Command {
     }
   }
 
-  /**
-   * Build a list of Products and nested Prices from the local Nodewood configuration.
-   *
-   * Products and Prices are sorted by ID.
-   *
-   * @return {Object}
-   */
-  getLocalConfig() {
-    try {
-      let config = readJsonSync(resolve(process.cwd(), 'app/config/stripe.json'));
-      config.products = sortBy(config.products, 'id').map((product) => {
-        return {
-          ...product,
-          prices: sortBy(product.prices, 'id'),
-        };
-      });
-      config.prices = flatMap(
-        config.products,
-        (product) => product.prices.map((price) => ({ product: product.id, ...price })),
-      ).filter((price) => price.product);
-
-      return config;
-    }
-    catch (error) {
-      if (error.code === 'ENOENT') {
-        console.log(chalk.red(`Could not find '${chalk.cyan(resolve(process.cwd(), 'app/config/stripe.json'))}'.`));
-      }
-      else {
-        console.log(chalk.red(error.message));
-      }
-
-      throw error;
-    }
-  }
-
-  /**
-   * Build a list of Products and nested Prices from the project's Stripe account.
-   *
-   * Products and Prices are sorted by ID.
-   *
-   * @return {Object}
-   */
-  async getRemoteConfig() {
-    return {
-      products: sortBy(await this.getStripeProductList(), 'id').map((product) => ({
-        id: product.id,
-        name: product.name,
-        description: product.description,
-        active: product.active,
-        metadata: product.metadata,
-      })),
-      prices: sortBy(await this.getStripePriceList(), 'id').map((price) => ({
-        id: price.id,
-        nickname: price.nickname,
-        active: price.active,
-        unit_amount: price.unit_amount,
-        currency: price.currency,
-        interval: price.recurring.interval,
-        interval_count: price.recurring.interval_count,
-        metadata: price.metadata,
-      })),
-    };
-  }
-
-  /**
-   * Get the full, unaltered list of products from Stripe.
-   *
-   * @return {Array}
-   */
-  async getStripeProductList() {
-    let productList = [];
-    let params = {};
-    let response;
-
-    do {
-      response = await stripe.products.list(params); // eslint-disable-line no-await-in-loop
-      params.starting_after = get(last(response.data), 'id');
-
-      productList = productList.concat(response.data);
-    } while (response.has_more);
-
-    return productList;
-  }
-
-  /**
-   * Get the full, unaltered list of prices from Stripe.
-   *
-   * @return {Array}
-   */
-  async getStripePriceList() {
-    let priceList = [];
-    let params = {};
-    let response;
-
-    do {
-      response = await stripe.prices.list(params); // eslint-disable-line no-await-in-loop
-      params.starting_after = get(last(response.data), 'id');
-
-      priceList = priceList.concat(response.data);
-    } while (response.has_more);
-
-    return priceList;
-  }
-
-  /**
-   * Calculate the differences between the local config and the remote config.
-   */
-  calculateDifferences() {
-    this.differences = {
-      products: {
-        new: this.getNewProductDifferences(),
-        updated: this.getUpdatedProductDifferences(),
-        deactivated: this.getDeactivatedProductDifferences(),
-      },
-      prices: {
-        new: this.getNewPriceDifferences(),
-        updated: this.getUpdatedPriceDifferences(),
-        deactivated: this.getDeactivatedPriceDifferences(),
-      },
-    };
-  }
-
-  /**
-   * Calculate the new products to be created on Stripe (& their prices).
-   *
-   * Any product in local configuration without an ID is new.
-   *
-   * @return {Array}
-   */
-  getNewProductDifferences() {
-    return this.localConfig.products.filter((product) => ! get(product, 'id'));
-  }
-
-  /**
-   * Calculate products to be updated on Stripe.
-   *
-   * @return {Array<Object>} - The ID of the products to be updated.
-   */
-  getUpdatedProductDifferences() {
-    return this.localConfig.products.map((product) => omit(product, 'prices')).filter((product) => {
-      if (! get(product, 'id')) {
-        return false;
-      }
-
-      const remoteProduct = last(this.remoteConfig.products.filter(
-        (remote) => remote.id === product.id,
-      ));
-
-      if (! remoteProduct) {
-        console.log(chalk.yellow(`Local product '${chalk.cyan(product.name)}' has an ID but does not exist remotely.`));
-        console.log(chalk.yellow('This could be because you deleted a product on Stripe, but not from your config.\n'));
-        return false;
-      }
-
-      // Different if there is more than one difference between the keys
-      return Object.keys(product)
-        .filter((key) => ! isEqual(product[key], remoteProduct[key]))
-        .length > 0;
-    });
-  }
-
-  /**
-   * Calculate products to be deactivated on Stripe.
-   *
-   * Any remote product that does not have a local product or whose local product isn't active
-   * should be deactivated.
-   *
-   * @return {Array<Object>} - The ID of the products to be deactivated.
-   */
-  getDeactivatedProductDifferences() {
-    return this.remoteConfig.products.filter((product) => {
-      const localProduct = last(this.localConfig.products.filter(
-        (local) => local.id === product.id,
-      ));
-
-      return (! localProduct || ! localProduct.active);
-    });
-  }
-
-  /**
-   * Calculate new prices for existing products to be created on Stripe.
-   *
-   * A new price is defined as a price without an ID in a local product that DOES have an ID.
-   *
-   * @return {Array<Object>}
-   */
-  getNewPriceDifferences() {
-    return this.localConfig.prices.filter((price) => ! price.id);
-  }
-
-  /**
-   * Calculate prices to be updated on Stripe.
-   *
-   * @return {Array<Object>}
-   */
-  getUpdatedPriceDifferences() {
-    return this.localConfig.prices.filter((price) => price.id).filter((price) => {
-      const remotePrice = last(this.remoteConfig.prices.filter((remote) => remote.id === price.id));
-
-      if (! remotePrice) {
-        console.log(chalk.yellow(`Local price '${chalk.cyan(price.id)}' has an ID but does not exist remotely.`));
-        console.log(chalk.yellow('This could be because you deleted a price on Stripe, but not from your config.\n'));
-        return false;
-      }
-
-      // Different if there is more than one difference between the keys
-      return Object.keys(price)
-        .filter((key) => ! isEqual(price[key], remotePrice[key]))
-        .length > 0;
-    });
-  }
-
-  /**
-   * Calculate prices to be deactivated on Stripe.
-   *
-   * @return {Array}
-   */
-  getDeactivatedPriceDifferences() {
-    return this.remoteConfig.prices.filter((price) => {
-      const localPrice = last(this.localConfig.prices.filter(
-        (local) => local.id === price.id,
-      ));
-
-      return (! localPrice || ! localPrice.active);
-    });
-  }
-
   async diff() {
-    console.log(this.differences.prices);
+    const differences = calculateDifferences(this.localConfig, this.remoteConfig);
+    console.log(differences.prices);
   }
 
   async sync() {
@@ -303,13 +79,7 @@ class StripeCommand extends Command {
   }
 
   async import() {
-    writeJsonSync(
-      resolve(process.cwd(), 'app/config/stripe.json'),
-      this.remoteConfig,
-      {
-        spaces: 2,
-      },
-    );
+    writeLocalConfig(this.remoteConfig);
 
     console.log(`${chalk.cyan(resolve(process.cwd(), 'app/config/stripe.json'))} has been created from existing Stripe configuration.`);
   }
---
+++ src/lib/stripe.js
@@ -0,0 +1,212 @@
+const chalk = require('chalk');
+const stripe = require('stripe')(process.env.STRIPE_SK);
+const { readJsonSync, writeJsonSync } = require('fs-extra');
+const { get, last, sortBy, omit, isEqual, flatMap } = require('lodash');
+const { resolve } = require('path');
+
+/**
+ * Build a list of Products and nested Prices from the local Nodewood configuration.
+ *
+ * Products and Prices are sorted by ID.
+ *
+ * @return {Object}
+ */
+function getLocalConfig() {
+  try {
+    let config = readJsonSync(resolve(process.cwd(), 'app/config/stripe.json'));
+    config.products = sortBy(config.products, 'id').map((product) => {
+      return {
+        ...product,
+        prices: sortBy(product.prices, 'id'),
+      };
+    });
+    config.prices = flatMap(
+      config.products,
+      (product) => product.prices.map((price) => ({ product: product.id, ...price })),
+    ).filter((price) => price.product);
+
+    return config;
+  }
+  catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log(chalk.red(`Could not find '${chalk.cyan(resolve(process.cwd(), 'app/config/stripe.json'))}'.`));
+    }
+    else {
+      console.log(chalk.red(error.message));
+    }
+
+    throw error;
+  }
+}
+
+function writeLocalConfig(config) {
+  writeJsonSync(
+    resolve(process.cwd(), 'app/config/stripe.json'),
+    config,
+    {
+      spaces: 2,
+    },
+  );
+}
+
+/**
+ * Build a list of Products and nested Prices from the project's Stripe account.
+ *
+ * Products and Prices are sorted by ID.
+ *
+ * @return {Object}
+ */
+async function getRemoteConfig() {
+  return {
+    products: sortBy(await getStripeProductList(), 'id').map((product) => ({
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      active: product.active,
+      metadata: product.metadata,
+    })),
+    prices: sortBy(await getStripePriceList(), 'id').map((price) => ({
+      id: price.id,
+      nickname: price.nickname,
+      active: price.active,
+      unit_amount: price.unit_amount,
+      currency: price.currency,
+      interval: price.recurring.interval,
+      interval_count: price.recurring.interval_count,
+      metadata: price.metadata,
+    })),
+  };
+}
+
+/**
+ * Get the full, unaltered list of products from Stripe.
+ *
+ * @return {Array}
+ */
+async function getStripeProductList() {
+  let productList = [];
+  let params = {};
+  let response;
+
+  do {
+    response = await stripe.products.list(params); // eslint-disable-line no-await-in-loop
+    params.starting_after = get(last(response.data), 'id');
+
+    productList = productList.concat(response.data);
+  } while (response.has_more);
+
+  return productList;
+}
+
+/**
+ * Get the full, unaltered list of prices from Stripe.
+ *
+ * @return {Array}
+ */
+async function getStripePriceList() {
+  let priceList = [];
+  let params = {};
+  let response;
+
+  do {
+    response = await stripe.prices.list(params); // eslint-disable-line no-await-in-loop
+    params.starting_after = get(last(response.data), 'id');
+
+    priceList = priceList.concat(response.data);
+  } while (response.has_more);
+
+  return priceList;
+}
+
+/**
+ * Calculate the differences between the local config and the remote config.
+ *
+ * @return {Object}
+ */
+function calculateDifferences(localConfig, remoteConfig) {
+  // When checking local products for updates, skip new products and omit the prices field
+  const localProductsForUpdateCheck = localConfig.products
+    .filter((product) => get(product, 'id'))
+    .map((product) => omit(product, 'prices'));
+
+  // When checking local prices for updates, skip new prices
+  const localPricesForUpdateCheck = localConfig.prices.filter((price) => price.id);
+
+  return {
+    products: {
+      new: getNewEntries(localConfig.products),
+      updated: getUpdatedEntries(localProductsForUpdateCheck, remoteConfig.products, 'product'),
+      deactivated: getDeactivatedEntries(localConfig.products, remoteConfig.products),
+    },
+    prices: {
+      new: getNewEntries(localConfig.prices),
+      updated: getUpdatedEntries(localPricesForUpdateCheck, remoteConfig.prices, 'price'),
+      deactivated: getDeactivatedEntries(localConfig.prices, remoteConfig.prices),
+    },
+  };
+}
+
+/**
+ * Calculate the new entries in a list.
+ *
+ * Any entry without an ID is new.
+ *
+ * @param {Array<Object>} entries - The entries to check.
+ *
+ * @return {Array}
+ */
+function getNewEntries(entries) {
+  return entries.filter((entry) => ! get(entry, 'id'));
+}
+
+/**
+ * Calculate updated entries in a list.
+ *
+ * @param {Array<Object>} localEntries - The local entries to compare.
+ * @param {Array<Object>} remoteEntries - The remote entries to compare against.
+ * @param {String} entryName - The name of the entry we're checking, for logging.
+ *
+ * @return {Array<Object>}
+ */
+function getUpdatedEntries(localEntries, remoteEntries, entryName) {
+  return localEntries.filter((localEntry) => {
+    const remoteEntry = last(remoteEntries.filter((remote) => remote.id === localEntry.id));
+
+    if (! remoteEntry) {
+      console.log(chalk.yellow(`Local ${entryName} '${chalk.cyan(localEntry.id)}' has an ID but does not exist remotely.`));
+      console.log(chalk.yellow(`This could be because you deleted a ${entryName} on Stripe, but not from your config.\n`));
+      return false;
+    }
+
+    // Different if there is more than one difference between the keys
+    return Object.keys(localEntry)
+      .filter((key) => ! isEqual(localEntry[key], remoteEntry[key]))
+      .length > 0;
+  });
+}
+
+/**
+ * Calculate entries to be deactivated on Stripe.
+ *
+ * Any remote entry that does not have a local entry or whose local entry isn't active
+ * should be deactivated.
+ *
+ * @param {Array<Object>} localEntries - The local entries to be checked.
+ * @param {Array<Object>} remoteEntries - The remote entries to be checked against.
+ *
+ * @return {Array<Object>} - The entries to be deactivated.
+ */
+function getDeactivatedEntries(localEntries, remoteEntries) {
+  return remoteEntries.filter((remote) => {
+    const localEntry = last(localEntries.filter((local) => local.id === remote.id));
+
+    return (! localEntry || ! localEntry.active);
+  });
+}
+
+module.exports = {
+  getLocalConfig,
+  writeLocalConfig,
+  getRemoteConfig,
+  calculateDifferences,
+};
